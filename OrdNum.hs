81ecc945ec91d26abaccfae4bcb290e001437ce4
merliborn <sourei.merlin@gmail.com>
2018-10-17 11:25:50 +0900

    split module

--- OrdNum.hs
+++ OrdNum.hs
@@ -4,6 +4,7 @@
 -- OrdNumPRで取り扱える順序数はω^ωより小さいものとする。
 
 module OrdNum where
+import Numeric.Natural
 
 class OrdNum a where
     normalize          :: a -> a
@@ -11,34 +12,7 @@ class OrdNum a where
     
     normalize = normalizeWithLevel 0
     
-    (:+), (:*), (:^) :: a -> a -> a
+    (.+), (.*), (.^) :: a -> a -> a
     
     toOrdNum :: Natural -> a
-
-module OrdNum.OrdNumPR where
-import Numeric.Natural
-
-data OrdShort = Zero | L Natural OrdShort
-  deriving(Eq, Read, Show)
-
-instance OrdNum OrdShort where
-    normalizeWithLevel n Zero    = Zero
-    normalizeWithLevel n (L k a) = if n> k then normalizeWithLevel n a
-                                           else L k (normalizeWithLevel k a)
-    
-    toOrdNum 0 = Zero
-    toOrdNum n = L 0 (toOrdNum pred n)
-    
-
-indexOf :: OrdShort -> Natural
-indexOf Zero    = 0
-indexOf (L k a) = k
-
-instance Ord OrdShort where
-    Zero    <= b       = True
-    (L m a) <= Zero    = False
-    (L m a) <= (L n b) = case compare m n of { LT -> True;
-                                               GT -> False;
-                                               EQ -> (normalize a) <= (normalize b) }
-
 
---
+++ OrdNumPR.hs
@@ -0,0 +1,31 @@
+module OrdNum.OrdNumPR where
+import Numeric.Natural
+import OrdNum
+
+data OrdShort = Zero | L Natural OrdShort
+  deriving(Eq, Read, Show)
+
+instance OrdNum OrdShort where
+    normalizeWithLevel n Zero    = Zero
+    normalizeWithLevel n (L k a) = if n> k then normalizeWithLevel n a
+                                           else L k (normalizeWithLevel k a)
+    
+    toOrdNum 0 = Zero
+    toOrdNum n = L 0 (toOrdNum (pred n))
+    
+    (.+) = undefined
+    (.*) = undefined
+    (.^) = undefined
+
+indexOf :: OrdShort -> Natural
+indexOf Zero    = 0
+indexOf (L k a) = k
+
+instance Ord OrdShort where
+    Zero    <= b       = True
+    (L m a) <= Zero    = False
+    (L m a) <= (L n b) = case compare m n of { LT -> True;
+                                               GT -> False;
+                                               EQ -> (normalize a) <= (normalize b) }
+
+
