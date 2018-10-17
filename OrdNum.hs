948301c0732d3d39d5079dfd86d7e80db2a5c19e
merliborn <sourei.merlin@gmail.com>
2018-10-17 10:10:28 +0900

    classify OrdNum

--- OrdNum.hs
+++ OrdNum.hs
@@ -3,16 +3,42 @@
 -- 
 -- OrdNumPRで取り扱える順序数はω^ωより小さいものとする。
 
-module OrdNumPR where
+module OrdNum where
 
+class OrdNum a where
+    normalize          :: a -> a
+    normalizeWithLevel :: Natural -> a -> a
+    
+    normalize = normalizeWithLevel 0
+    
+    (:+), (:*), (:^) :: a -> a -> a
+    
+    toOrdNum :: Natural -> a
 
-data OrdShort = Ordinal Natural | L Natural OrdShort
+module OrdNum.OrdNumPR where
+import Numeric.Natural
+
+data OrdShort = Zero | L Natural OrdShort
   deriving(Eq, Read, Show)
 
-normalize :: OrdShort -> OrdShort
-normalize = normalizeWithLevel 0
+instance OrdNum OrdShort where
+    normalizeWithLevel n Zero    = Zero
+    normalizeWithLevel n (L k a) = if n> k then normalizeWithLevel n a
+                                           else L k (normalizeWithLevel k a)
+    
+    toOrdNum 0 = Zero
+    toOrdNum n = L 0 (toOrdNum pred n)
+    
 
-normalizeWithLevel :: Natural -> OrdShort -> OrdShort
-normalizeWithLevel n (Ordinal m) = Ordinal m
-normalizeWithLevel n (L k a)     = if n>k then normalizeWithLevel n a
-                                          else L k (normalizeWithLevel k a)
+indexOf :: OrdShort -> Natural
+indexOf Zero    = 0
+indexOf (L k a) = k
+
+instance Ord OrdShort where
+    Zero    <= b       = True
+    (L m a) <= Zero    = False
+    (L m a) <= (L n b) = case compare m n of { LT -> True;
+                                               GT -> False;
+                                               EQ -> (normalize a) <= (normalize b) }
+
+
